4c251cbc9f176c46aba1bb9aa24277a39fd68c2d
Ran Hong <hongran@uw.edu>
2016-09-02 20:18:15 -0500

    adding new plot.C

--- FieldSolver/plot.C
+++ FieldSolver/plot.C
@@ -1,7 +1,8 @@
 {
   ifstream input;
-  input.open("output52.txt",ios::in);
-  double phi,B_read,B_cal;
+  input.open("output52full.txt",ios::in);
+  double phi,B_read,B_cal,B_R,B_Z,B_Phi;
+  int Nprb;
   TGraph *gB_read = new TGraph();
   gB_read->SetName("gB_read");
   gB_read->SetMarkerStyle(20);
@@ -14,11 +15,50 @@
   gB_cal->SetMarkerSize(0.3);
   gB_cal->SetMarkerColor(kRed);
   gB_cal->SetLineColor(kRed);
-  for (int i=0;i<7842;i++){
-    input>>phi>>B_read>>B_cal;
-    gB_read->SetPoint(i,phi,B_read);
-    gB_cal->SetPoint(i,phi,B_cal);
+  TGraph *gB_res = new TGraph();
+  gB_res->SetName("gB_res");
+  gB_res->SetMarkerStyle(21);
+  gB_res->SetMarkerSize(0.3);
+  gB_res->SetMarkerColor(kBlue);
+  gB_res->SetLineColor(kBlue);
+  double B0=0;
+  double B0_cal=0;
+  for (int i=0;i<196050;i++){
+    input>>phi>>Nprb>>B_read>>B_Z>>B_R>>B_Phi>>B_cal;
+    B0+=B_read;
+    B0_cal+=B_cal;
   }
+  input.close();
+  B0/=196050.0;
+  B0_cal/=196050.0;
+  cout <<"Average B = "<<B0<<endl;
+  cout <<"Average B_cal = "<<B0_cal<<endl;
+  cout <<"Diff B average = "<<(B0_cal-B0)/B0*1000000<<endl;
+  int iG=0;
+  double RMS1=0;
+  double RMS2=0;
+  input.open("output52full.txt",ios::in);
+  for (int i=0;i<196050;i++){
+    input>>phi>>Nprb>>B_read>>B_Z>>B_Phi>>B_R>>B_cal;
+    RMS1+=pow(B_read-B0,2.0);
+    RMS2+=pow(B_cal-B_read,2.0);
+    if (Nprb==1){
+      gB_read->SetPoint(iG,phi,B_read);
+      gB_cal->SetPoint(iG,phi,B_cal);
+      gB_res->SetPoint(iG,phi,(B_read-B_cal)/B0);
+      iG++;
+    }
+  }
+  RMS1/=196050.0;
+  RMS2/=196050.0;
+  RMS1=sqrt(RMS1);
+  RMS2=sqrt(RMS2);
+  RMS1/=B0;
+  RMS2/=B0;
+  RMS1*=1000000;
+  RMS2*=1000000;
+  cout << "Fluctuation = "<<RMS1<<" ppm"<<endl;
+  cout << "Fit Residual = "<<RMS2<<" ppm"<<endl;
   TCanvas c1;
   gB_read->Draw("APL");
   gB_read->SetTitle("B field measured vs fit");
@@ -34,4 +74,7 @@
   Lg->Draw("same");
 
   c1.SaveAs("BField.pdf");
+  
+  TCanvas c2;
+  gB_res->Draw("APL");
 }
