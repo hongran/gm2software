c0c3ba5e799582c322ae3122d3ad3e53673de5ae
hongran <hongran@uw.edu>
2016-09-05 20:00:51 -0500

    cuda fully developed

--- FieldSolver/cuda/calculatebfield.cu
+++ FieldSolver/cuda/calculatebfield.cu
@@ -14,11 +14,10 @@ July 10, 2016
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <TTree.h>
-#include <TFile.h>
-#include <thread>
+//#include <TTree.h>
+//#include <TFile.h>
 #include <iostream>
-#include <mutex>
+#include <cuda_runtime.h>
 
 /* hypergeometric function 1F2 */
 double h1f2(double a, double b, double c, double z);
@@ -40,27 +39,26 @@ typedef struct B_Struct{
 }B_Struct;
 
 using namespace std;
-//Globals
 
-double *cc, *cs, *sc, *ss, *lsigma;
-int **ccidx;
-int *midx;
-int *nidx;
-double **Tlegq;
-double **Tdleq;
-double zeta[30], eta[30];
-double ** dataread;
-
-B_Struct *B_Fit_array;
-B_Struct *B_ZFit_array;
-B_Struct *B_RFit_array;
-B_Struct *B_PhiFit_array;
-
-mutex mlock;
-
-void thread_call(const double rr,const double bzero,const int dim1,int tid,int blocksize,int nphi);
+__global__ void thread_call(const double *cc,const double *cs,const double *sc,const double *ss,const double *zeta,const double *eta,const double *Tlegq,const double *Tdleq,const int *midx,const int *nidx,const int*ccidx,const double *dataread,double *dataoutZ,double *dataoutR,double *dataoutPhi,const double rr,const double bzero,const int dim1,int nphi);
 
 int main () {
+  double *cc, *cs, *sc, *ss, *lsigma;
+  int *ccidx;
+  int *midx;
+  int *nidx;
+  double *Tlegq;
+  double *Tdleq;
+  double *zeta, *eta;
+  double * dataread;
+  double * dataoutZ;
+  double * dataoutR;
+  double * dataoutPhi;
+  cudaError_t err = cudaSuccess;
+
+  zeta = (double*)malloc(30*sizeof(double));
+  eta = (double*)malloc(30*sizeof(double));
+
     int nmax, mmax, nphi, dim1, dim2;
     FILE *input, *iname, *datafile, *datac , *output;
     int Nthread = 8;
@@ -78,7 +76,6 @@ int main () {
 //    int ccidx[mmax+2][nmax+2];
     double zt, et, zt0, bzero, br, bz, bphi, bsize;
     double difrms, dzrms, ffrms, rmsd;
-    double datareadold[30];
     double angles[30], radii[30], bfield[30][5];
     int i, j, id, idx, md, nd, mdx, ndx, prb, na, ma, qidx, vid;
     double datatmp, tp, tx, phi, wgt, iwgt, legq, dleq;
@@ -93,15 +90,10 @@ int main () {
     sc=(double *)malloc(sizeof*sc*(dim1+2));
     ss=(double *)malloc(sizeof*ss*(dim1+2));
     lsigma=(double *)malloc(sizeof*lsigma*(nmax+2));
-    ccidx=(int **)malloc((dim1+2)*sizeof(int*));
+    int ccidx_dim = dim1+2;
+    ccidx=(int *)malloc((dim1+2)*(dim1+2)*sizeof(int));
     if(ccidx==NULL){
         printf("out of memory\n");
-    }
-    for (i=0;i<(dim1+2);i++) {
-        ccidx[i]=(int *)malloc((dim1+2)*sizeof(int));
-        if (ccidx[i]==NULL) {
-            printf("out of memory\n");
-        }
     }
     midx=(int *)malloc((dim1+2)*sizeof(int*));
     nidx=(int *)malloc((dim1+2)*sizeof(int*));
@@ -144,7 +136,7 @@ int main () {
     for (nd=0;nd<=nmax;nd++) {
         for (md=0;md<=mmax;md++) {
             i+=1;
-            ccidx[md][nd]=i;
+            ccidx[md*ccidx_dim+nd]=i;
             midx[i]=md;
             nidx[i]=nd;
             cc[i]=0.; cs[i]=0.; sc[i]=0.; ss[i]=0.;
@@ -159,23 +151,23 @@ int main () {
         fscanf (iname, "%d \t %d \t %d", &id, &ma, &na); 
 //        fscanf (input, "%d \t %d \t %d \t %lg", &id, &ma, &na, &tp); 
         fscanf (input, "%lg", &tp); 
-        printf("%d, %d, %lg \n", id, ccidx[ma][na], tp);
+//        printf("%d, %d, %lg \n", id, ccidx[ma*ccidx_dim+na], tp);
 //        if (tp!=tp) printf("%d, %d, %lg \n", id, ccidx[ma][na], tp);
         if (id==1) {
 //            printf("cc \n");
-            cc[ccidx[ma][na]]=tp;
+            cc[ccidx[ma*ccidx_dim+na]]=tp;
         }
         else if (id==2) {
 //            printf("cs \n");
-            cs[ccidx[ma][na]]=tp;
+            cs[ccidx[ma*ccidx_dim+na]]=tp;
         }
         else if (id==3) {
 //            printf("sc \n");
-            sc[ccidx[ma][na]]=tp;
+            sc[ccidx[ma*ccidx_dim+na]]=tp;
         }
         else if (id==4) {
 //            printf("ss \n");
-            ss[ccidx[ma][na]]=tp;
+            ss[ccidx[ma*ccidx_dim+na]]=tp;
         }
         else {
             printf("error \n");
@@ -192,44 +184,49 @@ int main () {
     }
     nphi=nphi/26; // azimuthal angle + 25 probes 
     fclose(datac); // close data file used for counting 
-    //Allocate memory for parallel calculation
-    B_Fit_array = (B_Struct*)malloc(sizeof(B_Struct)*nphi);
-    B_ZFit_array = (B_Struct*)malloc(sizeof(B_Struct)*nphi);
-    B_RFit_array = (B_Struct*)malloc(sizeof(B_Struct)*nphi);
-    B_PhiFit_array = (B_Struct*)malloc(sizeof(B_Struct)*nphi);
 
     //Loading data first
-    dataread = (double **)malloc(sizeof(double*)*nphi);
-    for (i=0;i<nphi;i++){
-      dataread[i] = (double *)malloc(sizeof(double)*26);
-    }
+    dataread = (double *)malloc(sizeof(double)*26*nphi);
+    dataoutZ = (double *)malloc(sizeof(double)*26*nphi);
+    dataoutR = (double *)malloc(sizeof(double)*26*nphi);
+    dataoutPhi = (double *)malloc(sizeof(double)*26*nphi);
     double tmp;
     datafile = fopen ("data52.txt", "r");
     for (id=0;id<nphi;id++) { // loop over azimuthal slices 
       fscanf (datafile, "%lg", &tmp); // read azimuthal angle in degrees 
-      dataread[id][0]=tmp*M_PI/180.;
+      dataread[id*26]=tmp*M_PI/180.;
       for (prb=1;prb<=25;prb++){ // loop over 25 probes 
 	fscanf (datafile, "%lg", &tmp); 
-	dataread[id][prb]=tmp*0.001+61.7400000;
+	dataread[id*26+prb]=tmp*0.001+61.7400000;
       }
     }
     fclose(datafile);
+    //Zero output
+    for (id=0;id<nphi;id++) { // loop over azimuthal slices 
+      for (prb=1;prb<=25;prb++){ // loop over 25 probes 
+	dataoutZ[id*26+prb]=0.0;
+	dataoutR[id*26+prb]=0.0;
+	dataoutPhi[id*26+prb]=0.0;
+      }
+    }
     //Calculate legq and dleq table
-    Tlegq=(double **)malloc((dim1+1)*sizeof(double*));
-    Tdleq=(double **)malloc((dim1+1)*sizeof(double*));
-    for (int il=0;il<=dim1;il++) {
-      Tlegq[il]=(double *)malloc(26*sizeof(double));
-      Tdleq[il]=(double *)malloc(26*sizeof(double));
+    Tlegq=(double *)malloc((dim1+1)*26*sizeof(double));
+    Tdleq=(double *)malloc((dim1+1)*26*sizeof(double));
+
+    for (prb=0;prb<=25;prb++){ // loop over 25 probes 
+      for (i=0;i<=dim1;i++){
+	Tlegq[i*26+prb]=0.0;
+	Tdleq[i*26+prb]=0.0;
+      }
     }
     for (prb=1;prb<=25;prb++){ // loop over 25 probes 
-      zt=zeta[prb];  // zeta coordinate at probe 
       for (i=1;i<=dim1;i++){
 	int md=midx[i]; // m index 
 	int nd=nidx[i]; // n index 
 	/* LegendreQ at probe */
-	Tlegq[i][prb]=LegendreQ(md,nd,zeta[prb])/LegendreQ(md,nd,zt0);
+	Tlegq[i*26+prb]=LegendreQ(md,nd,zeta[prb])/LegendreQ(md,nd,zt0);
 	/* Derivative of LegendreQ at probe */
-	Tdleq[i][prb]=DLegendreQ(md,nd,zeta[prb])/LegendreQ(md,nd,zt0);
+	Tdleq[i*26+prb]=DLegendreQ(md,nd,zeta[prb])/LegendreQ(md,nd,zt0);
       }
     }
 
@@ -238,19 +235,105 @@ int main () {
     dzrms=0.;
     ffrms=0.;
 
+    //Allocate memory in device
+    //Function Talbe
+    double *d_legq = NULL;
+    double *d_dleq = NULL;
+    int f_size = sizeof(double)*26*(dim1+1);
+    err = cudaMalloc((void **)&d_legq, f_size);
+    err = cudaMalloc((void **)&d_dleq, f_size);
+
+    //Cooridinates
+    double *d_zeta = NULL;
+    double *d_eta = NULL;
+    int c_size = sizeof(double)*30;
+    err = cudaMalloc((void **)&d_zeta, c_size);
+    err = cudaMalloc((void **)&d_eta, c_size);
+
+    //data storage;
+    double *d_Data = NULL;
+    double *d_DataoutZ = NULL;
+    double *d_DataoutR = NULL;
+    double *d_DataoutPhi = NULL;
+    size_t sizeData = sizeof(double)*26*nphi;
+    err = cudaMalloc((void **)&d_Data, sizeData);
+    err = cudaMalloc((void **)&d_DataoutZ, sizeData);
+    err = cudaMalloc((void **)&d_DataoutR, sizeData);
+    err = cudaMalloc((void **)&d_DataoutPhi, sizeData);
+
+    //ccidx
+    int *d_ccidx = NULL;
+    int size_ccidx = (dim1+2)*(dim1+2)*sizeof(int);
+    err = cudaMalloc((void **)&d_ccidx, size_ccidx);
+
+    int *d_midx = NULL;
+    int size_midx = (dim1+2)*sizeof(int);
+    err = cudaMalloc((void **)&d_midx, size_midx);
+
+    int *d_nidx = NULL;
+    int size_nidx = (dim1+2)*sizeof(int);
+    err = cudaMalloc((void **)&d_nidx, size_nidx);
+
+    //Vectors
+    double *d_cc = NULL;
+    double *d_cs = NULL;
+    double *d_sc = NULL;
+    double *d_ss = NULL;
+    int d_size = sizeof(double)*(dim1+2);
+    err = cudaMalloc((void **)&d_cc, d_size);
+    err = cudaMalloc((void **)&d_cs, d_size);
+    err = cudaMalloc((void **)&d_sc, d_size);
+    err = cudaMalloc((void **)&d_ss, d_size);
+
+    if (err != cudaSuccess)
+    {
+      fprintf(stderr, "Failed to allocate device memory (error code %s)!\n", cudaGetErrorString(err));
+      exit(EXIT_FAILURE);
+    }
+
+
+    //copy matrix memory to device
+    err = cudaMemcpy(d_legq, Tlegq, f_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_dleq, Tdleq, f_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_zeta, zeta, c_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_eta, eta, c_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_Data, dataread, sizeData, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_DataoutZ, dataoutZ, sizeData, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_DataoutR, dataoutR, sizeData, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_DataoutPhi, dataoutPhi, sizeData, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_ccidx, ccidx, size_ccidx, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_midx, midx, size_midx, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_nidx, nidx, size_nidx, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_cc, cc, d_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_cs, cs, d_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_sc, sc, d_size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_ss, ss, d_size, cudaMemcpyHostToDevice);
+
+
+
     //Start parallel computing
-    thread *thread_array;
-    thread_array = new thread[Nthread]();
-    for (int i=0;i<Nthread;i++){
-      thread_array[i]=thread(thread_call,rr,bzero,dim1,i,nphi/Nthread+1,nphi);
+    dim3 DimBlock (16,16);
+    dim3 DimGrid (nphi/16+1, 26/16+1);
+    printf("CUDA kernel launch with %d blocks of %d threads\n", DimGrid.x * DimGrid.y, 256);
+
+    thread_call<<<DimGrid, DimBlock>>>(d_cc,d_cs,d_sc,d_ss,d_zeta,d_eta,d_legq,d_dleq,d_midx,d_nidx,d_ccidx,d_Data,d_DataoutZ,d_DataoutR,d_DataoutPhi,rr,bzero,dim1,nphi);
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+      fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
+      exit(EXIT_FAILURE);
     }
+    printf("GPU jobs done.\n");
+
+
     //End parallel computing
-    for (int i=0;i<Nthread;i++){
-      thread_array[i].join();
-    }
+    err = cudaMemcpy(dataoutZ, d_DataoutZ, sizeData, cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(dataoutR, d_DataoutR, sizeData, cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(dataoutPhi, d_DataoutPhi, sizeData, cudaMemcpyDeviceToHost);
     
     //open Root tree
-    TFile *outfile = new TFile("RootOut52.root","recreate");
+/*    TFile *outfile = new TFile("RootOut52.root","recreate");
     TTree *Tree_Measured = new TTree ("Tree_Measured", "Measured field");
     Tree_Measured->Branch("Phi",&phi,"Phi/D");
     Tree_Measured->Branch("BField",&B_Measure,"Prb1/D:Prb2:Prb3:Prb4:Prb5:Prb6:Prb7:Prb8:Prb9:Prb10:Prb11:Prb12:Prb13:Prb14:Prb15:Prb16:Prb17:Prb18:Prb19:Prb20:Prb21:Prb22:Prb23:Prb24:Prb25");
@@ -260,12 +343,13 @@ int main () {
     Tree_Fit->Branch("BFieldZ",&B_Fit,"Prb1/D:Prb2:Prb3:Prb4:Prb5:Prb6:Prb7:Prb8:Prb9:Prb10:Prb11:Prb12:Prb13:Prb14:Prb15:Prb16:Prb17:Prb18:Prb19:Prb20:Prb21:Prb22:Prb23:Prb24:Prb25");
     Tree_Fit->Branch("BFieldR",&B_Fit,"Prb1/D:Prb2:Prb3:Prb4:Prb5:Prb6:Prb7:Prb8:Prb9:Prb10:Prb11:Prb12:Prb13:Prb14:Prb15:Prb16:Prb17:Prb18:Prb19:Prb20:Prb21:Prb22:Prb23:Prb24:Prb25");
     Tree_Fit->Branch("BFieldPhi",&B_Fit,"Prb1/D:Prb2:Prb3:Prb4:Prb5:Prb6:Prb7:Prb8:Prb9:Prb10:Prb11:Prb12:Prb13:Prb14:Prb15:Prb16:Prb17:Prb18:Prb19:Prb20:Prb21:Prb22:Prb23:Prb24:Prb25");
+    */
     for (id=0;id<nphi;id++) { // loop over azimuthal slices 
-      phi=dataread[id][0];
+      phi=dataread[id*26];
       for (prb=1;prb<=25;prb++){ // loop over 25 probes 
-	br=B_RFit_array[id].Prb[prb-1];
-	bz=B_ZFit_array[id].Prb[prb-1];
-	bphi=B_PhiFit_array[id].Prb[prb-1];
+	bz=dataoutZ[id*26+prb];
+	br=dataoutR[id*26+prb];
+	bphi=dataoutPhi[id*26+prb];
 	bsize=sqrt(br*br+bz*bz+bphi*bphi);
 	//            printf("%Lf \n",bsize);
 	bfield[prb][4]=bsize;
@@ -276,21 +360,21 @@ int main () {
 	B_ZFit.Prb[prb-1]=bz;
 	B_RFit.Prb[prb-1]=br;
 	B_PhiFit.Prb[prb-1]=bphi;
-	B_Measure.Prb[prb-1]=dataread[id][prb];
+	B_Measure.Prb[prb-1]=dataread[id*26+prb];
 	//Output
-	fprintf(output,"%.17g %d %.17g %.17g %.17g %.17g %.17g\n",phi,prb,dataread[id][prb],bfield[prb][1],bfield[prb][2],bfield[prb][3],bfield[prb][4]);
-	rmsd=(dataread[id][prb]-bfield[prb][4])*(dataread[id][prb]-bfield[prb][4]);
+	fprintf(output,"%.17g %d %.17g %.17g %.17g %.17g %.17g\n",phi,prb,dataread[id*26+prb],bfield[prb][1],bfield[prb][2],bfield[prb][3],bfield[prb][4]);
+	rmsd=(dataread[id*26+prb]-bfield[prb][4])*(dataread[id*26+prb]-bfield[prb][4]);
 	difrms+=rmsd;
 	rmsd=rmsd/bzero/bzero;
-	dzrms+=(dataread[id][prb]-bz)*(dataread[id][prb]-bz);
-	ffrms+=(dataread[id][prb]-bzero)*(dataread[id][prb]-bzero);
+	dzrms+=(dataread[id*26+prb]-bz)*(dataread[id*26+prb]-bz);
+	ffrms+=(dataread[id*26+prb]-bzero)*(dataread[id*26+prb]-bzero);
       }
-      Tree_Measured->Fill();
-      Tree_Fit->Fill();
+     // Tree_Measured->Fill();
+     // Tree_Fit->Fill();
     }
-    Tree_Measured->Write();
-    Tree_Fit->Write();
-    outfile->Close();
+    //Tree_Measured->Write();
+    //Tree_Fit->Write();
+    //outfile->Close();
 
     difrms=difrms/bzero/bzero/nphi/25;
     dzrms=dzrms/bzero/bzero/nphi/25;
@@ -307,21 +391,14 @@ int main () {
     free(sc);
     free(ss);
     free(ccidx);
-    for (int il=0;il<=dim1;il++) {
-      free(Tlegq[il]);
-      free(Tdleq[il]);
-    }
+    free(midx);
+    free(nidx);
     free(Tlegq);
     free(Tdleq);
-    for (int il=0;il<nphi;il++) {
-      free(dataread[il]);
-    }
     free(dataread);
-
-    free(B_Fit_array);
-    free(B_ZFit_array);
-    free(B_RFit_array);
-    free(B_PhiFit_array);
+    free(dataoutZ);
+    free(dataoutR);
+    free(dataoutPhi);
 
     return 0;
 }
@@ -421,63 +498,57 @@ double etaf(double rho, double z, double r0){
 
 
 //Function for each thread call
-void thread_call(const double rr,const double bzero,const int dim1,int tid,int blocksize,int nphi){
-  for (int il=0;il<blocksize;il++) { // loop over azimuthal slices 
-    int id = tid*blocksize+il;
-    if (id>=nphi)return;
-    double phi=dataread[id][0];
-    for (int prb=1;prb<=25;prb++){ // loop over 25 probes 
-      double zt=zeta[prb];  // zeta coordinate at probe 
-      double et=eta[prb];  // eta coordinate at probe 
-      //            printf("%Lf, %Lf \n", zt, et);
-      double wgt=sqrt(cosh(zt)-cos(et)); // weight func in toroidal coordinates 
-      double br=0.; 
-      double bz=bzero; 
-      double bphi=0.;
-      for (int i=1;i<=dim1;i++){
-	int md=midx[i]; // m index 
-	int nd=nidx[i]; // n index 
-	//                printf("%d, %d \n", md, nd);
-	/* LegendreQ at probe */
-	double legq=Tlegq[i][prb];
-	/* Derivative of LegendreQ at probe */
-	double dleq=Tdleq[i][prb];
-	//                printf("(%d, %d, %Lf) : %Lf, %Lf \n", md,nd,zt,legq, dleq);
-	double phidz=cc[ccidx[md][nd]]*cos(nd*phi)*cos(md*et)+  \
-		     sc[ccidx[md][nd]]*cos(nd*phi)*sin(md*et)+  \
-		     cs[ccidx[md][nd]]*sin(nd*phi)*cos(md*et)+  \
-		     ss[ccidx[md][nd]]*sin(nd*phi)*sin(md*et);
-	phidz=phidz*(sinh(zt)/2./wgt*legq + wgt*dleq);
-	double phidp=-cc[ccidx[md][nd]]*sin(nd*phi)*cos(md*et)-  \
-		     sc[ccidx[md][nd]]*sin(nd*phi)*sin(md*et)+  \
-		     cs[ccidx[md][nd]]*cos(nd*phi)*cos(md*et)+  \
-		     ss[ccidx[md][nd]]*cos(nd*phi)*sin(md*et);
-	phidp=phidp*nd*wgt*legq;
-	double phide1=cc[ccidx[md][nd]]*cos(nd*phi)*cos(md*et)+  \
-		      sc[ccidx[md][nd]]*cos(nd*phi)*sin(md*et)+  \
-		      cs[ccidx[md][nd]]*sin(nd*phi)*cos(md*et)+  \
-		      ss[ccidx[md][nd]]*sin(nd*phi)*sin(md*et);
-	phide1=phide1*sin(et)/2./wgt*legq;
-	double phide2=-cc[ccidx[md][nd]]*cos(nd*phi)*sin(md*et)+  \
-		      sc[ccidx[md][nd]]*cos(nd*phi)*cos(md*et)-  \
-		      cs[ccidx[md][nd]]*sin(nd*phi)*sin(md*et)+  \
-		      ss[ccidx[md][nd]]*sin(nd*phi)*cos(md*et);
-	phide2=phide2*md*wgt*legq;
-	double phide=phide1+phide2;
-	br=br+sinh(zt)/rr*((1.-cosh(zt)*cos(et))/sinh(zt)*phidz-sin(et)*phide);
-	bz=bz+sinh(zt)/rr*(-sin(et)*phidz-(1.-cosh(zt)*cos(et))/sinh(zt)*phide);
-	bphi=bphi+(cosh(zt)-cos(et))/(rr*sinh(zt))*phidp;
-      }
-      double bsize=sqrt(br*br+bz*bz+bphi*bphi);
-      //            printf("%Lf \n",bsize);
-      B_Fit_array[id].Prb[prb-1]=bsize;
-      B_ZFit_array[id].Prb[prb-1]=bz;
-      B_RFit_array[id].Prb[prb-1]=br;
-      B_PhiFit_array[id].Prb[prb-1]=bphi;
+__global__ void thread_call(const double *cc,const double *cs,const double *sc,const double *ss,const double *zeta,const double *eta,const double *Tlegq,const double *Tdleq,const int *midx,const int *nidx,const int*ccidx,const double *dataread,double *dataoutZ,double *dataoutR,double *dataoutPhi,const double rr,const double bzero,const int dim1,int nphi){
+  //Get index
+  int id = blockDim.x * blockIdx.x + threadIdx.x;
+  int prb = blockDim.y * blockIdx.y + threadIdx.y;
+  int ccidx_dim = dim1+2;
+  if(id<nphi && prb<26 && prb>0){
+    double phi=dataread[id*26];
+    double zt=zeta[prb];  // zeta coordinate at probe 
+    double et=eta[prb];  // eta coordinate at probe 
+    //            printf("%Lf, %Lf \n", zt, et);
+    double wgt=sqrt(cosh(zt)-cos(et)); // weight func in toroidal coordinates 
+    double br=0.; 
+    double bz=bzero; 
+    double bphi=0.;
+    for (int i=1;i<=dim1;i++){
+      int md=midx[i]; // m index 
+      int nd=nidx[i]; // n index 
+      //                printf("%d, %d \n", md, nd);
+      /* LegendreQ at probe */
+      double legq=Tlegq[i*26+prb];
+      /* Derivative of LegendreQ at probe */
+      double dleq=Tdleq[i*26+prb];
+      //                printf("(%d, %d, %Lf) : %Lf, %Lf \n", md,nd,zt,legq, dleq);
+      double phidz=cc[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*cos(md*et)+  \
+		   sc[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*sin(md*et)+  \
+		   cs[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*cos(md*et)+  \
+		   ss[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*sin(md*et);
+      phidz=phidz*(sinh(zt)/2./wgt*legq + wgt*dleq);
+      double phidp=-cc[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*cos(md*et)-  \
+		   sc[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*sin(md*et)+  \
+		   cs[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*cos(md*et)+  \
+		   ss[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*sin(md*et);
+      phidp=phidp*nd*wgt*legq;
+      double phide1=cc[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*cos(md*et)+  \
+		    sc[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*sin(md*et)+  \
+		    cs[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*cos(md*et)+  \
+		    ss[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*sin(md*et);
+      phide1=phide1*sin(et)/2./wgt*legq;
+      double phide2=-cc[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*sin(md*et)+  \
+		    sc[ccidx[md*ccidx_dim+nd]]*cos(nd*phi)*cos(md*et)-  \
+		    cs[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*sin(md*et)+  \
+		    ss[ccidx[md*ccidx_dim+nd]]*sin(nd*phi)*cos(md*et);
+      phide2=phide2*md*wgt*legq;
+      double phide=phide1+phide2;
+      br=br+sinh(zt)/rr*((1.-cosh(zt)*cos(et))/sinh(zt)*phidz-sin(et)*phide);
+      bz=bz+sinh(zt)/rr*(-sin(et)*phidz-(1.-cosh(zt)*cos(et))/sinh(zt)*phide);
+      bphi=bphi+(cosh(zt)-cos(et))/(rr*sinh(zt))*phidp;
     }
-    mlock.lock();
-    printf("Local Slice %d (phi=%.17g) of %d done %f percent \n",il, phi, blocksize,double(il)/double(blocksize)*100 );
-    mlock.unlock();
+    dataoutZ[id*26+prb]=bz;
+    dataoutR[id*26+prb]=br;
+    dataoutPhi[id*26+prb]=bphi;
   }
 }
 
--- FieldSolver/run.sh
+++ FieldSolver/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 time ./makematrix > makematrixout52.log
 time ./solvematrix > solvematrix52.log
-./bfieldHong > bfield52.log
+time ./bfieldHong > bfield52.log
